c7339b97a02fb419e680e752098d8eda79e58fc2
Nikolay Pavlov <nikolaypavlov4@gmail.com>
2020-09-27 23:27:19 +0300

    fix #2

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-console.log(Notification.permission);
-
 let inputBtnWeb = document.querySelector('.input-btn-web');
-
+let inputBtnPhone = document.querySelector('.input-btn-phone');
 inputBtnWeb.addEventListener('click', () => {
     let emailInputWeb = document.querySelector('#form-input-web').value;
     if (emailInputWeb !== "" && emailInputWeb.includes("@") && emailInputWeb.includes(".")) {
         let emailNotification = document.createElement('div');
-        emailNotification.id = "notification";
+        emailNotification.id = "notification-web";
         emailNotification.innerHTML = `${emailInputWeb} has been submitted!`
+        document.body.appendChild(emailNotification);
+        setTimeout( function() {
+            emailNotification.remove();
+        }, 4000);
+    }
+})
+
+inputBtnPhone.addEventListener('click', () => {
+    let emailInputPhone = document.querySelector('#form-input-phone').value;
+    if (emailInputPhone !== "" && emailInputPhone.includes("@") && emailInputPhone.includes(".")) {
+        let emailNotification = document.createElement('div');
+        emailNotification.id = "notification-phone";
+        emailNotification.innerHTML = `${emailInputPhone} has been submitted!`
         document.body.appendChild(emailNotification);
         setTimeout( function() {
             emailNotification.remove();
